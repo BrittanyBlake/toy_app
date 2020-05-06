ed4fdd625aec84d1533511ecd68545339f1c53a3
Brittany Blake <brittany.blake49@gmail.com>
2020-05-06 17:48:18 +0200

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+   
   end
 
   # GET /users/new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :microposts
+    validates :name, length: { maximum: 140},
+              presence: true
+    validates :email, length: { maximum: 140},
+              presence: true
 end
